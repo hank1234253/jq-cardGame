f65cfc408a899923ba1e946f98b6424f051c6fc7
hank <hank1234253@gmail.com>
2023-07-05 12:51:26 +0800

    增加排名功能

---
+++ ajax/rank.php
@@ -0,0 +1,24 @@
+<?php
+    $dsn="mysql:host=localhost;charset=utf8;dbname=game";
+    $pdo=new PDO($dsn,"root","");
+
+    if(!empty($_POST['time'])){
+    $sql="select count(*) from `rank` where `time`<={$_POST['time']}";
+    $check=$pdo->query($sql)->fetchColumn();
+    if($check<10){
+        $check++;
+        $sql="INSERT INTO `rank`(`name`, `time`) VALUES ('{$_POST['name']}','{$_POST['time']}')";
+        $pdo->exec($sql);
+        $pdo->exec("DELETE FROM `rank` WHERE 1 ORDER BY time DESC LIMIT 1");
+    }
+}
+
+
+    $rows=$pdo->query("select * from `rank` order by `time` limit 10")->fetchAll();
+    foreach($rows as $row){
+        $tmp[]=["name"=>$row['name'],"time"=>$row['time']];
+    }
+
+
+    echo json_encode($tmp);
+?>
